a39bc8723a48d77535ce61566347d8123ad6196a
Dawson Sanders <dmsanders30@gmail.com>
2023-02-15 21:59:14 -0600

    add sample configs

---
+++ app/config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {  //This is the development property and is for development purposes
+        username: "postgres",
+        password: "password",
+        database: "mydatabase",
+        host: "localhost",
+        dialect: "postgres",
+    },
+};

0baae7f01843acd30b27eed1daf424b7f27a89c7
Dawson Sanders <dmsanders30@gmail.com>
2023-02-15 22:02:20 -0600

    sample structure of database

---
+++ app/models/model.js
@@ -0,0 +1,24 @@
+// module.exports = (sequelize, DataTypes) => {
+//     const Clinics = sequelize.define(
+//       "clinics",
+//       {
+//         name: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//         },
+//         zipCode: {
+//           type: DataTypes.INTEGER,
+//           allowNull: false,
+//         },
+//         city: {
+//           type: DataTypes.STRING,
+//           allowNull: false,
+//         },
+//       },
+//       {
+//         timestamps: false,
+//       }
+//     );
+  
+//     return Clinics;
+//   };

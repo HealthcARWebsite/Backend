570960920fd012665b9fb554252b23544f976388
Dawson Sanders <dmsanders30@gmail.com>
2023-02-15 22:03:16 -0600

    add middleware

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cors = require('cors');
 // Imports the bodyParser library
 const bodyParser = require('body-parser');
 
+// Imports the models folder 
+const db = require("./app/models");
+
 // Instantiates the app
 const app = express();
 
@@ -14,6 +17,20 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
+
+
+// Sets up database connection
+db.sequelize.sync()
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((err) => {
+        console.log("Failed to connect with database: " + err.message);
+    });
+
+
+
+
 
 // Assigns either the local machines predefined port or port 4001
 const PORT = process.env.PORT || 4001;

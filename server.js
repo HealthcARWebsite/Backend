95035bacb721d165052a734c2532e4b1ca637b35
Dawson Sanders <dmsanders30@gmail.com>
2023-02-15 21:18:05 -0600

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 // Imports the express library 
 const express = require('express');
 
+// Imports the cors library
+const cors = require('cors');
+
+// Imports the bodyParser library
+const bodyParser = require('body-parser');
+
 // Instantiates the app
 const app = express();
+
+// Middleware functions
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Assigns either the local machines predefined port or port 4001
 const PORT = process.env.PORT || 4001;

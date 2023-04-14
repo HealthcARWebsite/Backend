4d25b10c7b11c922d23f041621580be1bf5cbd5f
Dawson Sanders <dmsanders30@gmail.com>
2023-04-14 12:32:11 -0500

    more cors

--- app/controller.js
+++ app/controller.js
@@ -1,6 +1,32 @@
 const pool = require('../db.js');
 const queries = require('./queries.js');
 
+
+// Imports the express library 
+const express = require('express');
+
+// Instantiates the app
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+})
+
+
+
+
 // Imports the nodemailer library to send users emails
 const nodemailer = require('nodemailer');
 
